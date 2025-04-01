aaab07e994d6ce42afd7d1ace7b6c34bb2307d60
Andrew Fijalkowski <afijalkowski@harriscomputer.com>
2025-03-31 21:37:42 -0400

    correct benchmark file paths for new proj struct

--- Fhir.Fluid.Converter.Benchmark/Benchmarks/BenchmarkConstants.cs
+++ Fhir.Fluid.Converter.Benchmark/Benchmarks/BenchmarkConstants.cs
@@ -2,8 +2,8 @@
 {
     public static class BenchmarkConstants
     {
-        public const string SampleDataPath = @"C:\work\FluidCdaTest\data\SampleData\";
-        public const string DevTemplatesPath = @"C:\work\FluidCdaTest\FluidCdaTest.Benchmark\DevBranchTemplates\Ccda";
+        public const string SampleDataPath = @"C:\work\FHIR-Fluid-Converter\data\SampleData\";
+        public const string DevTemplatesPath = @"C:\work\FHIR-Fluid-Converter\Fhir.Fluid.Converter.Benchmark\DevBranchTemplates\Ccda";
         public const string TemplatesPath = @"C:\work\HAG-FHIR\HAG.FHIR.API\data\Templates\Ccda";
         public const string RootTemplatePath = @"C:\work\HAG-FHIR\HAG.FHIR.API\data\Templates\Ccda\CCD.liquid";
     }
